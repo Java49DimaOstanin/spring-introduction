cad09a15bb373c1f1a09d0d17ed37a2cf779d27e
DimaOstanin <xnadab@gmail.com>
2023-12-05 22:41:52 +0200

    CW #56 spring

--- src/main/java/telran/spring/WebApplication.java
+++ src/main/java/telran/spring/WebApplication.java
@@ -2,12 +2,14 @@ package telran.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringIntroductionApplication {
+//@ComponentScan(basePackages = {"telran"})
+public class WebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIntroductionApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 
 }
