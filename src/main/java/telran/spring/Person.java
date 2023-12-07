b272a1fbfe87899ee3d72a8bb8e2460129290cb5
DimaOstanin <xnadab@gmail.com>
2023-12-07 20:51:58 +0200

    CW #58 Web Services aspects (validation, exceptions handling, logging)

--- src/main/java/telran/spring/Person.java
+++ src/main/java/telran/spring/Person.java
@@ -1,5 +1,8 @@
 package telran.spring;
 
-public record Person(long id, String Name, String city) {
+import jakarta.validation.constraints.*;
+
+public record Person(long id,@Pattern (regexp = "[A-Z][a-z]{2,}")String name,
+		@NotEmpty String city, /*TODO validation */String email,/*TODO israel phone valedetion*/ String phone) {
 
 }
--- src/main/java/telran/spring/WebApplication.java
+++ src/main/java/telran/spring/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"telran"})
+@ComponentScan(basePackages = {"telran"})
 public class WebApplication {
 
 	public static void main(String[] args) {
