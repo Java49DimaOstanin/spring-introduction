a196d0ebacb6df68733edc7aca63b0bba4ae3450
DimaOstanin <xnadab@gmail.com>
2023-12-07 18:12:26 +0200

    CW #57 CRUD Restful service

--- src/main/java/telran/spring/controller/GreetingsController.java
+++ src/main/java/telran/spring/controller/GreetingsController.java
@@ -15,4 +15,19 @@ public class GreetingsController {
 	String getGreetings(@PathVariable long id) {
 		return greetingsService.getGreetings(id);
 	}
+	//TODO update following control end point methods for HW #57
+	@PostMapping
+	String addName(@RequestBody IdName idNAme) {
+	 return	greetingsService.addName(idNAme);
+	}
+	@PutMapping
+	String updateName(@RequestBody IdName idName) {
+		return greetingsService.updateName(idName);
+	}
+	@DeleteMapping("{id}")
+	String deleteName(@PathVariable long id) {
+		return greetingsService.deleteName(id);
+	}
+	//TOD
+	//end points for getting person by ID and getting persons by city see service
 }
--- src/main/java/telran/spring/service/GreetingsServiceImpl.java
+++ src/main/java/telran/spring/service/GreetingsServiceImpl.java
@@ -1,17 +1,51 @@
 package telran.spring.service;
 
+
+
 import java.util.*;
 
 import org.springframework.stereotype.Service;
+
+
 @Service
 public class GreetingsServiceImpl implements GreetingsService {
-	Map<Long,String> greetingsMap = Map.of(123l,"David",124l,"Sara",125l,"Rivka");
+	Map<Long,String> greetingsMap = new HashMap<>(Map.of(123l,"David",124l,"Sara",125l,"Rivka"));
 	
 	
 	@Override
 	public String getGreetings(long id) {
 		String name = greetingsMap.getOrDefault(id, "Unknown Guest");
 		return "Hello, " + name;
+	}
+
+
+	@Override
+	public String addName(IdName idName) {
+		String name = greetingsMap.putIfAbsent(idName.id(), idName.name());
+		if(name != null) {
+			 throw new IllegalStateException(idName.id() +" name with gived id alredy exists");
+		}
+		return idName.name();
+	}
+
+
+	@Override
+	public String deleteName(long id) {
+		String name = greetingsMap.remove(id);
+		if(name == null) {
+			throw new IllegalStateException(id + " not found");
+		}
+		return null;
+	}
+
+
+	@Override
+	public String updateName(IdName idName) {
+		if(!greetingsMap.containsKey(idName.id())) {
+			throw new IllegalStateException(idName.id() + " not found");
+		}
+		greetingsMap.put(idName.id(), idName.name() );
+		return idName.name();
 	}
 
 }
